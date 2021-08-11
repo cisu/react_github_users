5f703fdf2393dfdf5646c210aed90cefc495f9cb
george <geor@webdi.com>
2021-08-11 23:40:08 +0300

    react router error

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <Route path='/' exact>
-        <Dashboard></Dashboard>
-      </Route>
+      <Switch>
+        <Route path='/' exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
 
-      <Route path="/login">
-        <Login />
-      </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   );
 }
